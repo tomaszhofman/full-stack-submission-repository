aab515eb1ed16cea1efa47599bd2ac81a6b36cb1
tomaszhofman <tomasz.k.hofman@gmail.com>
2021-07-01 19:43:46 +0200

    delete files

--- part4/bloglist-frontend/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const baseUrl = '/api/login';
-
-const login = async (credintentials) => {
-  const response = await axios.post(baseUrl, credintentials);
-  return response.data;
-};
-
-export default { login };
